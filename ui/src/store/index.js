8066c970a650076a8903c3e91643c8ba178cbda6
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-05-16 15:48:35 +0200

    Initial commit

---
+++ ui/src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    appName: "kickstart", // TODO
+    instanceName: "",
+    instanceLabel: "",
+    core: null,
+  },
+  mutations: {
+    setInstanceName(state, instanceName) {
+      state.instanceName = instanceName;
+    },
+    setInstanceLabel(state, instanceLabel) {
+      state.instanceLabel = instanceLabel;
+    },
+    setCore(state, core) {
+      state.core = core;
+    },
+  },
+  actions: {
+    setInstanceNameInStore(context, instanceName) {
+      context.commit("setInstanceName", instanceName);
+    },
+    setInstanceLabelInStore(context, instanceLabel) {
+      context.commit("setInstanceLabel", instanceLabel);
+    },
+    setCoreInStore(context, core) {
+      context.commit("setCore", core);
+    },
+  },
+  modules: {},
+});
