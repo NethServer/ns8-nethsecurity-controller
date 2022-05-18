36978ceb13703ea798acaa835536151b30202dd0
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-05-18 12:37:36 +0000

    create-module: setup proxy

--- build-images.sh
+++ build-images.sh
@@ -29,9 +29,9 @@ buildah add "${container}" ui/dist /ui
 # Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
 buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@any:routeadm" \
-    --label="org.nethserver.tcp-ports-demand=4" \
+    --label="org.nethserver.tcp-ports-demand=5" \
     --label="org.nethserver.rootfull=1" \
-    --label="org.nethserver.images=ghcr.io/nethserver/nextsec-vpn:$tag ghcr.io/nethserver/nextsec-api:$tag ghcr.io/nethserver/nextsec-ui:$tag docker.io/library/traefik:v2.6" \
+    --label="org.nethserver.images=ghcr.io/nethserver/nextsec-vpn:$tag ghcr.io/nethserver/nextsec-api:$tag ghcr.io/nethserver/nextsec-ui:$tag ghcr.io/nethserver/nextsec-proxy:$tag" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
