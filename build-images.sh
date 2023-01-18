e7f0c432f3f144439883080cf956ea4707b22c0c
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-01-18 15:04:34 +0100

    build: updated node version, fixed building process for node > 18

--- build-images.sh
+++ build-images.sh
@@ -17,11 +17,15 @@ container=$(buildah from scratch)
 # Reuse existing nodebuilder-nextsec-controller container, to speed up builds
 if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-nextsec-controller; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-nextsec-controller -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-nextsec-controller -v "${PWD}:/usr/src:Z" docker.io/library/node:18.13.0-alpine
 fi
 
 echo "Build static UI files with node..."
-buildah run nodebuilder-nextsec-controller sh -c "cd /usr/src/ui && yarn install && yarn build"
+buildah run \
+    --workingdir "/usr/src/ui" \
+    --env "NODE_OPTIONS=--openssl-legacy-provider" \
+    nodebuilder-nextsec-controller \
+    sh -c "yarn install --frozen-lockfile && yarn build"
 
 # Add imageroot directory to the container image
 buildah add "${container}" imageroot /imageroot
@@ -50,7 +54,7 @@ images+=("${repobase}/${reponame}")
 #
 
 #
-# Setup CI when pushing to Github. 
+# Setup CI when pushing to Github.
 # Warning! docker::// protocol expects lowercase letters (,,)
 if [[ -n "${CI}" ]]; then
     # Set output value for Github Actions
