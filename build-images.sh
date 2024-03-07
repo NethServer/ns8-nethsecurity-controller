24b3e0c02d3000bb259487bb677a0d25bdc3ea2b
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2024-03-07 14:40:22 +0100

    build: add webssh container

--- build-images.sh
+++ build-images.sh
@@ -9,11 +9,41 @@ images=()
 repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
 reponame="nethsecurity-controller"
-tag=${IMAGE_TAG:-0.0.1}
+controller_version=0.0.1
 promtail_version=2.7.1
 loki_version=2.9.4
 prometheus_version=2.50.1
 grafana_version=10.3.3
+webssh_version=1.6.2
+
+# Create a new empty container for webssh
+echo "Build webssh container" # from https://github.com/huashengdun/webssh
+mkdir -p webssh_tmp/build
+pushd webssh_tmp
+wget https://github.com/huashengdun/webssh/archive/refs/tags/v${webssh_version}.tar.gz -O webssh.tar.gz
+tar xvzf webssh.tar.gz -C build --strip-components=1
+pushd build
+webssh=$(buildah from docker.io/python:3-alpine)
+buildah add "${webssh}" . /code
+buildah run \
+    --workingdir '/code' \
+    ${webssh} \
+    sh -c "apk add --no-cache libc-dev libffi-dev gcc && \
+    pip install -r requirements.txt --no-cache-dir && \
+    apk del gcc libc-dev libffi-dev && \
+    addgroup webssh && \
+    adduser -Ss /bin/false -g webssh webssh && \
+    chown -R webssh:webssh /code
+    "
+popd
+popd
+rm -rf webssh_tmp
+# Commit the image
+buildah config --workingdir '/code'  --entrypoint='["python", "run.py"]'  "${webssh}"
+buildah commit "${webssh}" "${repobase}/webssh"
+
+# Append the image URL to the images array
+images+=("${repobase}/webssh")
 
 # Create a new empty container image
 container=$(buildah from scratch)
@@ -37,8 +67,8 @@ buildah add "${container}" ui/dist /ui
 # Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
 buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@any:routeadm node:tunadm" \
-    --label="org.nethserver.tcp-ports-demand=9" \
-    --label="org.nethserver.images=ghcr.io/nethserver/nethsecurity-vpn:$tag ghcr.io/nethserver/nethsecurity-api:$tag ghcr.io/nethserver/nethsecurity-ui:$tag ghcr.io/nethserver/nethsecurity-proxy:$tag docker.io/grafana/promtail:$promtail_version docker.io/grafana/loki:$loki_version docker.io/prom/prometheus:v$prometheus_version docker.io/grafana/grafana:$grafana_version" \
+    --label="org.nethserver.tcp-ports-demand=10" \
+    --label="org.nethserver.images=ghcr.io/nethserver/nethsecurity-vpn:$controller_version ghcr.io/nethserver/nethsecurity-api:$controller_version ghcr.io/nethserver/nethsecurity-ui:$controller_version ghcr.io/nethserver/nethsecurity-proxy:$controller_version docker.io/grafana/promtail:$promtail_version docker.io/grafana/loki:$loki_version docker.io/prom/prometheus:v$prometheus_version docker.io/grafana/grafana:$grafana_version ghcr.io/nethserver/webssh:${IMAGETAG:-latest}" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
