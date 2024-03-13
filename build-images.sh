d5bf293c02da8c56ce72263a3b180a6239ac512a
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2024-03-13 14:20:25 +0100

    build: bump controller version to 0.0.3

--- build-images.sh
+++ build-images.sh
@@ -9,7 +9,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
 reponame="nethsecurity-controller"
-controller_version=0.0.2
+controller_version=0.0.3
 promtail_version=2.7.1
 loki_version=2.9.4
 prometheus_version=2.50.1
