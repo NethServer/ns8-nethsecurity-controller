29ae69a9d21cfc9098e77a17b078ba0e4ff0cd77
Andrea Leardini <andrea.leardini@nethesis.it>
2025-06-12 10:22:38 +0200

    fix: pass new prop to NsBackupCard & upgrade node (#87)

--- build-images.sh
+++ build-images.sh
@@ -59,7 +59,7 @@ container=$(buildah from scratch)
 # Reuse existing nodebuilder-nethsecurity-controller container, to speed up builds
 if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-nethsecurity-controller; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-nethsecurity-controller -v "${PWD}:/usr/src:Z" docker.io/library/node:18.20.8-alpine
+    buildah from --name nodebuilder-nethsecurity-controller -v "${PWD}:/usr/src:Z" docker.io/library/node:22.16.0-slim
 fi
 
 echo "Build static UI files with node..."
