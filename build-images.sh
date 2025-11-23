d3db280da00b3d859038d72d0406432ff0ba86e6
gsanchietti <804596+gsanchietti@users.noreply.github.com>
2025-11-23 01:17:29 +0000

    build(deps): nethsecurity-controller package: update nethsecurity-controller package (automated)

--- build-images.sh
+++ build-images.sh
@@ -9,7 +9,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
 reponame="nethsecurity-controller"
-controller_version="2.1.1"
+controller_version="2.2.0"
 promtail_image="docker.io/grafana/promtail:3.6.1"
 loki_image="docker.io/grafana/loki:2.9.17"
 prometheus_image="docker.io/prom/prometheus:v3.7.3"
